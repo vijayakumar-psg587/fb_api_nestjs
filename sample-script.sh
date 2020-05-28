9dee898bfec52932ea2ae95371cfc6b13f232681
vijayakumar_psg587 <vijay.psg587@gmail.com>
2020-05-28 17:37:15 +0530

    removed unwanted files

--- sample-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "hello"
-
-# try reading the env file
-# inject or set the env
-
-# execute this command at the very end
-docker-compose -f deployments/docker/docker-compose.yml build
